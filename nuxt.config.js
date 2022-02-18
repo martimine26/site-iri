a15e267b5e5617feb641e2e15529e952aaaf2120
yaroslav <85133193+martimine26@users.noreply.github.com>
2022-02-18 20:10:29 +0300

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,8 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { name: 'yandex-verification', content: '4d78135e19b7ee94'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/logotip.ico' }
